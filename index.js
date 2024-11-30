0dd451ae62168d73636c46bf4569b3846a30529c
Omi <141710962+KrisTotebag@users.noreply.github.com>
2024-11-30 08:44:33 +0100

    Javascript functionality added

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 //Calculator Program
 
 const display = document.getElementById("display");
-
-function appendToDisplay(input){
+const validInput = /^[0-9+\-*/().]$/;
+const lastChar = display.value.slice(-1);
+//add function to window to put in global scope
+window.appendToDisplay = function(input){
+    const display = document.getElementById('display');
     display.value += input;
+}
+
+
+     // Prevent multiple consecutive operators
+    if (/^[+\-*/]$/.test(input) && /^[+\-*/]$/.test(lastChar)) {
+        return;
+    }
+
+    if (validInput.test(input)) {
+        display.value += input;
+    }
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try {
+       display.value = eval(display.value); 
+    } catch (error) {
+        display.value =  "Error detected,please press clear to restart";
+    }
 }
